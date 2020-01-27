8eca9fa4411982136b184bd3b2e30cdbcd3b2720
Tim de Jong <i@timdj.nl>
2020-01-27 11:59:06 +0100

    initial import

---
+++ body_class_toggle/README.md
@@ -0,0 +1,8 @@
+Body class toggle
+===============
+Super simple Drupal 8 module which will use javascript to toggle class on body ('.dark-mode' and .'light-mode').
+The latest settings will be saved in a cookie.
+
+To use add a button on link with id:
+
+ ***theme-toggler***
---
+++ body_class_toggle/js/body_class_toggle.script.js
@@ -0,0 +1,28 @@
+(function ($, Drupal, drupalSettings) {
+  'use strict';
+  Drupal.behaviors.bodyToggle = {
+    attach: function (context, settings) {
+      $('body', context).once('boddyToggle').append('<button id="theme-toggler">Toggle</button>');
+
+      if($.cookie('bodyclass')) {
+        $('body').addClass( $.cookie('bodyclass') );
+      }
+      else{
+        $('body').addClass('light-mode'); // set default theme. No need to set via php now
+      }
+
+      $('#theme-toggler', context).once('bodyToggle').click(function(){
+        if ($('body').hasClass( 'light-mode')){
+          $('body').removeClass('light-mode').addClass('dark-mode');
+          $.cookie('bodyclass', 'dark-mode');
+
+        }
+        else  {
+          $('body').removeClass('dark-mode').addClass('light-mode');
+          $.cookie('bodyclass', 'light-mode');
+        }
+      });
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings);
